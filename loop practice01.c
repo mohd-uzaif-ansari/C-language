989849486010561b268aeb79ea89dda3b428f572
Mohd Uzaif Ansari <mohammaduzaif0111@gmail.com>
2024-09-10 07:42:34 +0530

    Add files via upload
    This is my first programm on loops topic (while loop) .

---
+++ loop practice01.c
@@ -0,0 +1,13 @@
+// ~ mohd-uzaif-ansari
+
+#include <stdio.h>
+int main()
+{
+    int i = 0;
+    while (i < 5)
+    {
+        printf("%d Mohammad Uzaif\n", i);
+        i = i + 1;
+    }
+    return 0;
+}
