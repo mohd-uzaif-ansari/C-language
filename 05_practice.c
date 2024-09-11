d980dc8d02ab080989e164cc8bf6702e8f8e9bd5
Mohd Uzaif Ansari <mohammaduzaif0111@gmail.com>
2024-09-11 23:51:59 +0530

    Add files via upload

--- 05_practice.c
+++ 05_practice.c
@@ -1,19 +1,9 @@
-// ~ mohd-uzaif-ansari
-#include <stdio.h>
+#include<stdio.h>
 
 int main(){
-    int a=4, b=2, c=116, d=32;
-    if(a>b && a>c && a>d){
-        printf("The greatest of all is %d", a);
-    }
-    else if(b>a && b>c && b>d){
-        printf("The greatest of all is %d", b);
-    }
-    else if(c>a && c>b && c>d){
-        printf("The greatest of all is %d", c);
-    }
-    else if(d>a && d>c && d>b){
-        printf("The greatest of all is %d", d);
-    }
+    int a = 5;
+    int b = 4;
+    int c = a+b;
+    printf("the sum is : %d",c);
     return 0;
 }
