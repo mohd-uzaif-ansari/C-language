57908d60b7e37c4fb19f6e5fa2ccf3c709d80db9
Mohd Uzaif Ansari <mohammaduzaif0111@gmail.com>
2024-09-13 07:06:59 +0530

    Add files via upload

---
+++ do_whileloop.c
@@ -0,0 +1,13 @@
+// ~mohd-uzaif-ansari   
+#include <stdio.h>
+
+int main()
+{
+    int i = 0;
+    do
+    {
+        printf("The value of 1 is %d\n", i);
+        i++;
+    } while (i < 4);
+    return 0;
+}
