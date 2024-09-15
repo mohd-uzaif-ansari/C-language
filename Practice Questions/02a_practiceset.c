4ce72e59e33c461ec8fc4e6dfd2d85c1d49c9e1e
Mohd Uzaif Ansari <mohammaduzaif0111@gmail.com>
2024-09-15 23:54:11 +0530

    Add files via upload

---
+++ Practice Questions/02a_practiceset.c
@@ -0,0 +1,13 @@
+// mohd-uzaif-ansari
+#include <stdio.h>
+
+int main()
+{
+    // calculating area of circle
+    float radius, area, pi = 3.14159;
+    printf("Enter the radius : ");
+    scanf("%f", &radius);
+    area = pi * radius * radius;
+    printf("Area of circe is : %.3f", area);
+    return 0;
+}
