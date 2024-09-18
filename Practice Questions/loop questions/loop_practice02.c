6725546d702f9fa31c41d967ef01dc928f3b4998
Mohd Uzaif Ansari <mohammaduzaif0111@gmail.com>
2024-09-18 22:52:02 +0530

    Add files via upload

---
+++ Practice Questions/loop questions/loop_practice02.c
@@ -0,0 +1,17 @@
+// mohd-uzaif-ansari
+
+#include <stdio.h>
+int main (){
+
+   /* Write a program to print natural numbers from 10 to 20 when initial loop
+counter is initialized to 0.
+*/
+    int i = 0;
+    while(i<=20){
+        if(i>=10){
+            printf("Natural number %d\n",i);
+        }
+        i=i+1;
+    }
+    return 0;
+}
