122e5e27aeabe965dd1f18284e73d434f1b9de67
Mohd Uzaif Ansari <mohammaduzaif0111@gmail.com>
2024-09-18 22:23:49 +0530

    Add files via upload

---
+++ Practice Questions/loop questions/dowhile_quiz.c
@@ -0,0 +1,11 @@
+//  ~mohd-uzaif-ansari
+#include <stdio.h>
+int main (){
+    int n, i=1;
+    scanf("%d", &n);
+    do{
+        printf(" %d ",i);
+        i++;
+    }while(i<=n);
+    return 0;
+}
